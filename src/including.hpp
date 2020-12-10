628701b88064db60cfd8b1fb0d127d28804d47df
hitmoh <71852649+hitmoh@users.noreply.github.com>
2020-12-10 18:31:21 +0100

    Add files via upload

---
+++ src/including.hpp
@@ -0,0 +1,25 @@
+/*  use this file for inclusions*/
+
+
+
+
+#ifndef __INCLUDING_HPP__
+#define __INCLUDING_HPP__
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "entity.hpp"
+#include "ezevent.hpp"
+#include "ezsdl.hpp"
+#include "images.hpp"
+#include "sprit.hpp"
+
+
+
+
+#endif
